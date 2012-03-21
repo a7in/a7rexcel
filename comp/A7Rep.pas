07a15b05d529004c92400a2112073868f6eff32e
a7in <a7@a7in.com>
2012-03-21 12:56:21 +0200

    The current state of the component

---
+++ comp/A7Rep.pas
@@ -0,0 +1,173 @@
+unit A7Rep;
+
+interface
+
+uses
+  SysUtils, Classes, Variants, ComObj, StdCtrls, Forms;
+
+type
+  TA7Progress = class
+  private
+    F: TForm;
+    L1: TLabel;
+    L2: TLabel;
+  protected
+  public
+    procedure Line(p: integer);
+    constructor Create(AOwner: TComponent);
+    destructor Destroy; override;
+  end;
+
+  TA7Rep = class(TComponent)
+  private
+    Excel, TemplateSheet: Variant;
+
+    Progress: TA7Progress;
+
+    CurrentLine: integer;
+    FirstBandLine, LastBandLine: integer; // last paste band position
+  protected
+  public
+    procedure OpenTemplate(FileName: string);
+    procedure PasteBand(BandName: string);
+    procedure SetValue(VarName: string; Value: Variant);
+    procedure Show;
+    destructor Destroy; override;
+  published
+  end;
+
+procedure Register;
+
+implementation
+
+const
+  MaxBandLines = 300; // max template lines count
+  MaxBandColumns = 15; // max template column count
+
+procedure Register;
+begin
+  RegisterComponents('A7rexcel', [TA7Rep]);
+end;
+
+{ TWcReport }
+
+destructor TA7Rep.Destroy;
+begin
+
+  inherited;
+end;
+
+procedure TA7Rep.OpenTemplate(FileName: string);
+begin
+  Excel := CreateOleObject('Excel.Application');
+  Excel.Workbooks.Open(FileName, True, True);
+  TemplateSheet := Excel.Workbooks[1].Sheets[1];
+  Excel.DisplayAlerts := False; // for prevent error in SetValue procedure, where VarName not fount for replace
+  CurrentLine := 1;
+
+  Progress := TA7Progress.Create(Self);
+  Application.ProcessMessages;
+
+end;
+
+procedure TA7Rep.PasteBand(BandName: string);
+var
+  i: integer;
+  v, Range: Variant;
+begin
+
+  // find band in template
+  FirstBandLine := 0; LastBandLine := 0;
+  i := CurrentLine;
+  while ((LastBandLine = 0) and (i < CurrentLine + MaxBandLines)) do begin
+    v := Variant(TemplateSheet.Cells[i, 1].Value);
+    if (varType(v) = varOleStr) and (FirstBandLine = 0) then begin
+      if v = BandName then begin // the start of band
+        FirstBandLine := i;
+      end;
+    end;
+    if (FirstBandLine <> 0) then begin
+      if not ((varType(v) = varOleStr) and (v = BandName)) then LastBandLine := i - 1;
+    end;
+    inc(i);
+  end;
+
+  Range := TemplateSheet.Rows[IntToStr(FirstBandLine) + ':' + IntToStr(LastBandLine)];
+  Range.Copy;
+  Range := TemplateSheet.Rows[IntToStr(CurrentLine) + ':' + IntToStr(CurrentLine)];
+  Range.Insert;
+
+  // delete band name from result lines 
+  for i := CurrentLine to CurrentLine + (LastBandLine - FirstBandLine) do begin
+    TemplateSheet.Cells[i, 1].Value := '';
+  end;
+  CurrentLine := CurrentLine + (LastBandLine - FirstBandLine) + 1;
+  // new band position in report
+  FirstBandLine := CurrentLine - (LastBandLine - FirstBandLine) - 1;
+  LastBandLine := CurrentLine - 1;
+  Progress.Line(CurrentLine);
+end;
+
+procedure TA7Rep.SetValue(VarName: string; Value: Variant);
+var Range: Variant;
+  s: string;
+begin
+  s := Value;
+  Range := TemplateSheet.Rows[IntToStr(FirstBandLine) + ':' + IntToStr(LastBandLine)];
+  Range.Replace(VarName, s);
+end;
+
+procedure TA7Rep.Show;
+var
+  Range: Variant;
+begin
+  // delete the template from result report
+  Range := TemplateSheet.Rows[IntToStr(CurrentLine) + ':' + IntToStr(CurrentLine + MaxBandLines)];
+  Range.Delete;
+
+  Excel.Visible := true;
+  Progress.Free;
+end;
+
+{ TA7ProgressForm }
+
+constructor TA7Progress.Create(AOwner: TComponent);
+begin
+  F := TForm.Create(nil);
+  F.Width := 150;
+  F.Height := 80;
+  F.Position := poScreenCenter;
+  F.BorderStyle := bsNone;
+  F.FormStyle := fsStayOnTop;
+  F.Color := $FFFFFF;
+  L1 := TLabel.Create(F);
+  L1.Parent := F;
+  L1.Left := 15;
+  L1.Width := 100;
+  L1.Alignment := taCenter;
+  L1.Top := 20;
+  L1.Caption := 'Building report';
+  L2 := TLabel.Create(F);
+  L2.Parent := F;
+  L2.Left := 30;
+  L2.Width := 100;
+  L2.Alignment := taCenter;
+  L2.Top := 40;
+  F.Show;
+end;
+
+destructor TA7Progress.Destroy;
+begin
+  L1.Free;
+  L2.Free;
+  F.Free;
+end;
+
+procedure TA7Progress.Line(p: integer);
+begin
+  L2.Caption := 'Line: ' + IntToStr(p);
+  Application.ProcessMessages;
+end;
+
+end.
+
